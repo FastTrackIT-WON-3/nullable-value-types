335f48dc368d9e4d4742a325d2e8f72787ff0dd4
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-03-23 19:01:01 +0200

    Initial project structure; exploring nullable types properties and methods

---
+++ NullableValueTypes/NullableValueTypes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NullableValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool? flag = true;
+            PrintNullableTypeDetails(flag, true);
+
+            int? number = null;
+            PrintNullableTypeDetails(number, -10);
+        }
+
+        private static void PrintNullableTypeDetails<T>(T? value, T defaultValue)
+            where T: struct
+        {
+            Console.WriteLine("*******************************************");
+            Console.WriteLine($"value={value}");
+            Console.WriteLine($"value.HasValue={value.HasValue}");
+            if (value.HasValue)
+            {
+                Console.WriteLine($"value.Value={value.Value}");
+            }
+
+            Console.WriteLine($"value.GetValueOrDefault()={value.GetValueOrDefault()}");
+            Console.WriteLine($"value.GetValueOrDefault(defaultValue)={value.GetValueOrDefault(defaultValue)}");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine();
+        }
+    }
+}
